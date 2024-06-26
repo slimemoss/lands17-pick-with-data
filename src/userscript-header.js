9d58dffda2160b155a1ff663dcf3b30215a752b6
slimemoss <common.tken@gmail.com>
2024-06-26 13:05:50 +0900

    開発用のタグを削除
    tampermonkey側のみにつけとけばいいやつだった。

--- src/userscript-header.js
+++ src/userscript-header.js
@@ -1,9 +1,7 @@
 // ==UserScript==
-// @name react template
+// @name 17lands Pick with CardData
 // @version 1.0.0
 // @namespace http://tampermonkey.net/
-// @description react template
 // @author slimemoss
 // @match https://www.17lands.com/draft/*
-// @require      file:///home/slimemoss/work/hobby/mtg/userscript/react/dist/react-userscripts.user.js
 // ==/UserScript==
