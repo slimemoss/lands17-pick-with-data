30afcfa59fd0e925a9dc7da76e95081ea81d3740
slimemoss <common.tken@gmail.com>
2024-07-14 20:39:36 +0900

    update mh3 data

--- src/userscript-header.js
+++ src/userscript-header.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name 17lands Pick with CardData
-// @version 1.0.3
+// @version 1.0.4
 // @namespace http://tampermonkey.net/
 // @author slimemoss
 // @homepage https://github.com/slimemoss/lands17-pick-with-data
