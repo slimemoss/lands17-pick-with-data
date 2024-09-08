46adb8d0556374424718b15d2f4fd4f14353f87b
slimemoss <common.tken@gmail.com>
2024-09-09 04:51:57 +0900

    blb更新

--- src/userscript-header.js
+++ src/userscript-header.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name 17lands Pick with CardData
-// @version 1.0.5
+// @version 1.0.6
 // @namespace http://tampermonkey.net/
 // @author slimemoss
 // @homepage https://github.com/slimemoss/lands17-pick-with-data
