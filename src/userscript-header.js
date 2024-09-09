4583e4368286cc97835730da55fce67ab069dd79
slimemoss <common.tken@gmail.com>
2024-09-10 00:06:23 +0900

    userscripts取得先の自動ビルド対応漏れ

--- src/userscript-header.js
+++ src/userscript-header.js
@@ -4,6 +4,6 @@
 // @namespace http://tampermonkey.net/
 // @author slimemoss
 // @homepage https://github.com/slimemoss/lands17-pick-with-data
-// @updateURL https://github.com/slimemoss/lands17-pick-with-data/raw/master/dist/react-userscripts.user.js
+// @updateURL https://github.com/slimemoss/lands17-pick-with-data/raw/deploy/dist/react-userscripts.user.js
 // @match https://www.17lands.com/draft/*
 // ==/UserScript==
